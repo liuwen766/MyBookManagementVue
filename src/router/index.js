b55b70718852cfe547595b1a4dd26d699cdc491d
liuwen766 <780442619@qq.com>
2020-02-21 20:39:53 +0800

    图书管理项目：2.book管理页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Book from '../views/Book.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/book',
+    name: '书',
+    component: Book
   },
   {
     path: '/about',
